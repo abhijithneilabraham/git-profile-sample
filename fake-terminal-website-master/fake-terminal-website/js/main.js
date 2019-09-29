88860bfc9d4a156199d99c24a6e8e7dd9cbe03d2
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-09-29 18:09:43 +0530

    Update fake-terminal-website-master/fake-terminal-website/js/main.js

--- fake-terminal-website-master/fake-terminal-website/js/main.js
+++ fake-terminal-website-master/fake-terminal-website/js/main.js
@@ -12,7 +12,7 @@ var configs = (function () {
         }
     };
     Singleton.defaultOptions = {
-        general_help: "You are granted 6 wishes. You can choose one of them",
+        general_help: "You are granted 13 wishes. You can choose one of them",
         ls_help: "Show the treasures I have kept open for you.",
         cat_help: "Open the treasures.Enter 'treasure [filename]' ",
         whoami_help: "Who was I in the past Life?",
@@ -67,11 +67,11 @@ var files = (function () {
         }
     };
     Singleton.defaultOptions = {
-        "about.txt": "This website was made using only pure JavaScript with no extra libraries.\nI made it dynamic so anyone can use it, just download it from GitHub and change the config text according to your needs.\nIf you manage to find any bugs or security issues feel free to email me: luisbraganca@protonmail.com",
+        "about": "Yo, I use a human name to blend in Earth. Open the other treasures to know more about him",
         "getting_started.txt": "First, go to js/main.js and replace all the text on both singleton vars.\n- configs: All the text used on the website.\n- files: All the fake files used on the website. These files are also used to be listed on the sidenav.\nAlso please notice if a file content is a raw URL, when clicked/concatenated it will be opened on a new tab.\nDon't forget also to:\n- Change the page title on the index.html file\n- Change the website color on the css/main.css\n- Change the images located at the img folder. The suggested sizes are 150x150 for the avatar and 32x32/16x16 for the favicon.",
-        "contact.txt": "mail@example.com",
-        "social_network_1.txt": "https://www.socialite.com/username/",
-        "social_network_2.txt": "https://example.com/profile/9382/"
+        "Mail": "abhijithneilabrahampk@gmail.com",
+        "Github": "https://github.com/abhijithneilabraham/",
+        "LinkedIn": "https://www.linkedin.com/in/abhijith-neil-abraham-765165141/"
     };
     return {
         getInstance: function (options) {
